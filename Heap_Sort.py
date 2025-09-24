e6bbf2caa8779f5868f5d73dc4c329745e4d6290
Kim Jiah <2024184004@tukorea.ac.kr>
2025-09-24 13:37:30 +0900

    Heapify를 구현하고 1칸 진행

--- Heap_Sort.py
+++ Heap_Sort.py
@@ -5,13 +5,36 @@ from pyvisalgo import HeapSortVisualizer as Visualizer
 #from pyvisalgo import Dummy as Visualizer
 from time import time
 
+def heapify(root, size):
+    lc = root * 2 + 1
+    if lc >= size: return
+    child = lc
+    rc = root * 2 + 2
+    if rc < size:
+        vis.compare(rc, lc)
+        if array[rc] > array[lc]:
+            child = rc
+
+    vis.compare(root, child)
+    if array[root] < array[child]:
+        vis.swap(root, child)
+        array[root], array[child] = array[child], array[root]
+        heapify(child, size)
+
 def main():
     print('before', array)
     count = len(array)
-    print('after', array)
     vis.build_tree()
 
+    last_parent_index = count // 2 - 1
+    if True:
+        n = last_parent_index
+        vis.set_root(n)
+        heapify(n, count)
 
+    print('after', array)
+
+    
 if __name__=='__main__':
     seed('Hello')
     vis = Visualizer('Heap Sort')
