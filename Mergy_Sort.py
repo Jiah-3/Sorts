04f7aba0cb53ef9b65e383a4ad403fd6fbeababa
Kim Jiah <2024184004@tukorea.ac.kr>
2025-10-02 13:47:30 +0900

    정렬된 두 배열로 부터 하나씩 꺼내어 비교하면서 정렬 완성

--- Mergy_Sort.py
+++ Mergy_Sort.py
@@ -11,7 +11,42 @@ def main():
     array[mid:] = sorted(array[mid:])
 
     vis.push(0, mid-1, count-1)
+    merge(0, mid, count-1)
     print('after :', array)
+
+def merge(left, right, end):
+    merged = []
+    vis.start_merge(merged, False, left)
+    l, r = left, right
+    while l < right and r <= end:
+        vis.compare(l, r)
+        if array[l] <= array[r]:
+            merged.append(array[l])
+            vis.add_to_merged(l, True)
+            l += 1
+        else:
+            merged.append(array[r])
+            vis.add_to_merged(r, False)
+            r += 1
+
+    while l < right:
+        merged.append(array[l])
+        vis.add_to_merged(l, True)
+        l += 1
+    while r <= end:
+        merged.append(array[r])
+        vis.add_to_merged(r, False)
+        r += 1
+
+    vis.end_merge()
+
+    l = left
+    for n in merged:
+        array[l] = n
+        l += 1
+        # vis.erase_merged()
+
+    vis.pop()
 
 if __name__ == '__main__':
     seed('Hello')
