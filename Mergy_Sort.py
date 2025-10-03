a55ca98087688efc6e1e314ff8be49ab59b8d92b
Kim Jiah <2024184004@tukorea.ac.kr>
2025-10-03 14:15:37 +0900

    MergeSortVisualizer 사용으로 원상복귀 / Excel 업데이트

--- Mergy_Sort.py
+++ Mergy_Sort.py
@@ -1,81 +1,68 @@
-#from pyvisalgo import MergeSortVisualizer as Visualizer
+from pyvisalgo import MergeSortVisualizer as Visualizer
 # from pyvisalgo import Dummy as visualizer
 from time import time
 from random import randint, seed, shuffle
-#from data_unsorted import numbers
-from data_unsorted_a_lot import numbers
 
 def main():
-    #print('before:', array)
+    print('before:', array)
     count = len(array)
     mergeSort(0, count-1)
-    #print('after :', array)
+    print('after :', array)
 
 def insertionSort(left, right):
-  # print(f'B:{array[left:right+1]=} {left=} {right=}')
-  for i in range(left + 1, right + 1):
-    v = array[i]
-    j = i - 1
-    while j >= left and array[j] > v:
-      array[j+1] = array[j]
-      # print(f'-:{array[left:right+1]=}')
-      j -= 1
-    # print(f'{i=} {j=} {v=}')
-    array[j+1] = v
-    # print(f'=:{array[left:right+1]=} {j=}')
-  # print(f'A:{array[left:right+1]=}')
-
+    for i in range(left + 1, right + 1):
+        v = array[i]
+        vis.mark_end(i, v)
+        j = i - 1
+        while j >= left and array[j] > v:
+            vis.shift(j)
+            array[j+1] = array[j]
+            print(f'-:{array[left:right+1]=}')
+            j -= 1
+        print(f'{i=} {j=} {v=}')
+        vis.insert(i, j+1)
+        array[j+1] = v
 
 def mergeSort(left, right):
     if right <= left: return
     if right == left + 1:
-        if array[right] < array[left]:
-            #vis.compare(left, right)
-            if array[left] > array[right]:
-                #vis.swap(left, right
-                array[left], array[right] = array[right], array[left]
-            return
+        insertionSort(left, right)
+        return
     mid = (left + right) // 2
-    #vis.push(left, mid, right)
+    vis.push(left, mid, right)
     mergeSort(left, mid)
     mergeSort(mid+1, right)
     merge(left, mid+1, right)
-    #vis.pop()
+    vis.pop()
 
 def merge(left, right, end):
     merged = []
-    #vis.start_merge(merged, False, left)
+    vis.start_merge(merged, False, left)
     l, r = left, right
     while l < right and r <= end:
-        #vis.compare(l, r)
+        vis.compare(l, r)
         if array[l] <= array[r]:
             merged.append(array[l])
-            #vis.add_to_merged(l, True)
+            vis.add_to_merged(l, True)
             l += 1
         else:
             merged.append(array[r])
-            #vis.add_to_merged(r, False)
+            vis.add_to_merged(r, False)
             r += 1
 
     while l < right:
         merged.append(array[l])
-        #vis.add_to_merged(l, True)
+        vis.add_to_merged(l, True)
         l += 1
     while r <= end:
         merged.append(array[r])
-        #vis.add_to_merged(r, False)
+        vis.add_to_merged(r, False)
         r += 1
 
-    #vis.end_merge()
+    vis.end_merge()
 
     array[left:end+1] = merged
 
-    #l = left
-    #for n in merged:
-        #array[l] = n
-        #l += 1
-
-        # vis.erase_merged()
 
 ''' 성능 측정          orig slice 2only bugfx MgIns
 count=100    elapsed=0.000 0.000 0.000 0.000 0.000
@@ -104,25 +91,6 @@ count=1000000 elapsed=4.284 3.283 3.573 3.237 3.231
 
 if __name__ == '__main__':
     seed('Hello')
-
-    counts = [
-        100, 1000, 2000, 3000, 4000, 5000,
-        6000, 7000, 8000, 9000, 10000, 15000,
-        20000, 30000, 40000, 50000,
-        100000, 200000, 300000, 400000, 500000,
-        1000000, 2000000, 3000000, 4000000, 5000000,
-    ]
-    for count in counts:
-        # array = number[:count]
-        # shuffle(array)
-        mmm = count * 10 + randint(1, 1000)
-        array = [ randint(1, mmm) for _ in range(count)]
-        #print('before:', array)
-        startedOn = time()
-        main()
-        elapsed = time() - startedOn
-        print(f'{count=:<6d} {elapsed=:.3f}')
-    exit()
     vis = Visualizer('Merge Sort')
     while True:
         count = randint(20, 40)
