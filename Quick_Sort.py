a1e93f70fd4afa9bb14a2d2b87f6e3305bb8b3b1
Kim Jiah <2024184004@tukorea.ac.kr>
2025-10-04 19:17:53 +0900

    남은 원소가 4개 이하일 때에는 insertion sort 진행.

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -13,11 +13,8 @@ def main():
 def quicksort(left, right):
     #if left == right: vis.fix(left)
     if left >= right: return
-    if right == left + 1:
-        # vis.compare(left, right)
-        if array[left] > array[right]:
-            # vis.swap(left, right)
-            array[left], array[right] = array[right], array[left]
+    if right < left + 4:
+        insertionsort(left, right)
         return
     #vis.push(left, right)
     pivot = partition(left, right)
@@ -25,6 +22,18 @@ def quicksort(left, right):
     quicksort(left, pivot-1)
     quicksort(pivot+1, right)
     #vis.pop()
+
+def insertionsort(left, right):
+    for i in range(left + 1, right + 1):
+        v = array[i]
+        # vis.mark_end(i, v)
+        j = i - 1
+        while j >= left and array[j] > v:
+            # vis.shift(j)
+            array[j + 1] = array[j]
+            j -= 1
+        # vis.insert(i, j+1)
+        array[j + 1] = v
 
 def partition(left, right):
 
@@ -76,33 +85,33 @@ def partition(left, right):
     return q
 
 '''성능 측정
-#Quick Sort           Normal  2-swap
-count=100     elapsed= 0.000  0.000
-count=1000    elapsed= 0.001  0.001
-count=2000    elapsed= 0.003  0.003
-count=3000    elapsed= 0.004  0.003
-count=4000    elapsed= 0.006  0.005
-count=5000    elapsed= 0.007  0.006
-count=6000    elapsed= 0.009  0.006
-count=7000    elapsed= 0.022  0.007
-count=8000    elapsed= 0.017  0.009
-count=9000    elapsed= 0.026  0.010
-count=10000   elapsed= 0.029  0.013
-count=15000   elapsed= 0.025  0.017
-count=20000   elapsed= 0.042  0.023
-count=30000   elapsed= 0.063  0.039
-count=40000   elapsed= 0.148  0.090
-count=50000   elapsed= 0.147  0.079
-count=100000  elapsed= 0.256  0.203
-count=200000  elapsed= 0.600  0.378
-count=300000  elapsed= 0.802  0.623
-count=400000  elapsed= 1.125  0.852
-count=500000  elapsed= 1.645  1.093
-count=1000000 elapsed= 3.360  2.297
-count=2000000 elapsed= 8.011  6.157
-count=3000000 elapsed=12.169  9.965
-count=4000000 elapsed=15.897  13.828
-count=5000000 elapsed=20.592  16.930
+#Quick Sort           Normal  2-swap  4-Ins
+count=100     elapsed= 0.000  0.000   0.000
+count=1000    elapsed= 0.001  0.001   0.001
+count=2000    elapsed= 0.003  0.003   0.002
+count=3000    elapsed= 0.004  0.003   0.005
+count=4000    elapsed= 0.006  0.005   0.005
+count=5000    elapsed= 0.007  0.006   0.006
+count=6000    elapsed= 0.009  0.006   0.007
+count=7000    elapsed= 0.022  0.007   0.009
+count=8000    elapsed= 0.017  0.009   0.012
+count=9000    elapsed= 0.026  0.010   0.012
+count=10000   elapsed= 0.029  0.013   0.014
+count=15000   elapsed= 0.025  0.017   0.039
+count=20000   elapsed= 0.042  0.023   0.043
+count=30000   elapsed= 0.063  0.039   0.077
+count=40000   elapsed= 0.148  0.090   0.068
+count=50000   elapsed= 0.147  0.079   0.096
+count=100000  elapsed= 0.256  0.203   0.218
+count=200000  elapsed= 0.600  0.378   0.405
+count=300000  elapsed= 0.802  0.623   0.805
+count=400000  elapsed= 1.125  0.852   1.071
+count=500000  elapsed= 1.645  1.093   1.478
+count=1000000 elapsed= 3.360  2.297   2.679
+count=2000000 elapsed= 8.011  6.157   7.123
+count=3000000 elapsed=12.169  9.965   9.634
+count=4000000 elapsed=15.897  13.828  13.284
+count=5000000 elapsed=20.592  16.930  16.680
 '''
 if __name__ == '__main__':
     seed('Hello')
