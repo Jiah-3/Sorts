bcdd00849acccc86f81b5493295f355e05dee950
Kim Jiah <2024184004@tukorea.ac.kr>
2025-11-08 23:03:35 +0900

    차례대로 출력

--- partial_knapsack.py
+++ partial_knapsack.py
@@ -4,3 +4,11 @@ with open('data_knapsack.json') as f:
 print(f'Before: {mats=}')
 mats.sort(key=lambda m:m['price']/m['weight'], reverse=True)
 print(f'After : {mats=}')
+
+cap = 85
+items = []
+weight_sum = 0
+price_sum = 0
+
+for mat in mats:
+  print(f'{mat=}')
